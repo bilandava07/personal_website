04d0c10b8f8037ea9666c885782996c7b1ce80e4
David Bilan <bilandavid07@gmail.com>
2025-09-20 12:13:25 +0200

    Navigation bar is almost done now. Stays blurred while on hero page, with text color overriden to be white. Added support for mobile devices through aside opened by hamburger menu svg (cross svg to close the menu todo next). Styles just like the navbar by adjusting itself to the hero page and smoothly transitioning to the usual mode.

---
+++ static/js/sidebar_script.js
@@ -0,0 +1,19 @@
+const sidebar = document.getElementById("sidebar")
+const sidebar_toggle = document.getElementById("sidebar_toggle");
+
+sidebar_toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("show_sidebar");
+  sidebar_toggle.classList.toggle("show_sidebar");
+});
+
+// Close sidebar when clicking outside
+document.addEventListener("click", (e) => {
+  if (sidebar.classList.contains("show_sidebar") &&
+      sidebar_toggle.classList.contains("show_sidebar") &&
+      !sidebar.contains(e.target) &&
+      !sidebar_toggle.contains(e.target) &&
+      !theme_switch.contains(e.target)) {
+    sidebar.classList.remove("show_sidebar");
+    sidebar_toggle.classList.remove("show_sidebar");
+  }
+});
