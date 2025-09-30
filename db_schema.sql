3189141b42a6cc77bb30b2e94606e897b247f892
David Bilan <bilandavid07@gmail.com>
2025-09-30 11:21:49 +0200

    Updated db_schema to reflect current structure of the databse

--- db_schema.sql
+++ db_schema.sql
@@ -1,6 +1,11 @@
+'''Initiallt used for planning to makes drafts of the tables, actual table definitions in flask_app.py'''
+
 CREATE TABLE IF NOT EXISTS trips(
     trip_id INTEGER PRIMARY KEY AUTOINCREMENT,
     trip_name TEXT NOT NULL,
+    trip_video_filename TEXT,
+    trip_slug TEXT NOT NULL,
+    trip_description TEXT NOT NULL,
     start_time TEXT NOT NULL,
     avg_speed FLOAT NOT NULL,
     max_speed FLOAT NOT NULL,
@@ -10,26 +15,14 @@ CREATE TABLE IF NOT EXISTS trips(
     total_ascent FLOAT NOT NULL,
     total_descent FLOAT NOT NULL,
     geojson_filename TEXT NOT NULL
-
-
 )
-
 
 CREATE TABLE IF NOT EXISTS trips_images(
     image_id INTEGER PRIMARY KEY AUTOINCREMENT,
     trip_id INTEGER NOT NULL,
     image_filename TEXT NOT NULL,
     is_main BOOLEAN DEFAULT 0,
-    FOREIGN KEY (trip_id)  REFERENCES trips(id)
-
+    image_width INT NOT NULL,
+    image_height INT NOT NULL,
+    FOREIGN KEY (trip_id)  REFERENCES trips(trip_id)
 )
-
-
-
--- Querry to get the names and main images of all tripsj
-SELECT trips.trip_name, trips_images.image_path 
-FROM trips
-LEFT JOIN trips_images ON trips.id = trips_images.trip_id 
-AND trips_images.is_main = 1
-
- 
